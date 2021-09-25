5b5cfb19eed9726bed04bcd0b7a2d2d5e836c4ed
souyez-capsule <91373775+souyez-capsule@users.noreply.github.com>
2021-09-25 07:34:43 -0400

    Add files via upload

---
+++ zapper_api-bare.py
@@ -0,0 +1,25 @@
+import requests
+
+API_KEY = '96e0cc51-a62e-42ca-acee-910ea7d2a241'
+
+#--- function to get token balances from zapper fi ---#
+def token_balances(wallet_address):
+    api_url = 'https://api.zapper.fi/v1/protocols/tokens/balances?addresses%5B%5D='+wallet_address+'&network=ethereum&api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241'
+    r = requests.get(api_url)
+    api_data = r.json()
+    tokens = api_data[wallet_address]['products'][0]['assets']
+
+    data = dict()
+
+    for this_token in tokens:
+        this_currency = this_token['symbol']
+        this_balance = this_token['balance']
+        data[this_currency] = this_balance
+
+    return data
+#--- end function to get token balances from zapper fi ---#
+
+wallet = '0x0000000000000000000000000000000000000000'
+
+balances = token_balances(wallet)
+print(balances)
